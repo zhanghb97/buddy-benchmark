970f6f06a540c3ce8c6f29fce3c9c54db215a35d
zhanghb97 <hongbin2019@iscas.ac.cn>
2022-02-15 14:39:38 +0800

    [Action] Test LLVM IR generation.

--- tests/Actions/BuildBuddyBenchmark.sh
+++ tests/Actions/BuildBuddyBenchmark.sh
@@ -1,8 +1,15 @@
-mkdir build
-cd build
-cmake -G Ninja .. \
-    -DIMAGE_PROCESSING_BENCHMARKS=ON \
-    -DDEEP_LEARNING_BENCHMARKS=ON \
-    -DOpenCV_DIR=$PWD/../opencv/build/ \
-    -DBUDDY_OPT_BUILD_DIR=$PWD/../buddy-mlir/build/ 
-ninja
+# mkdir build
+# cd build
+# cmake -G Ninja .. \
+#     -DIMAGE_PROCESSING_BENCHMARKS=ON \
+#     -DDEEP_LEARNING_BENCHMARKS=ON \
+#     -DOpenCV_DIR=$PWD/../opencv/build/ \
+#     -DBUDDY_OPT_BUILD_DIR=$PWD/../buddy-mlir/build/ 
+# ninja
+
+./buddy-mlir/build/bin/buddy-opt ./benchmarks/ImageProcessing/BuddyConv2D.mlir \
+  -conv-vectorization="strip-mining=256" \
+  -lower-affine -convert-scf-to-cf -convert-vector-to-llvm \
+  -convert-memref-to-llvm -convert-std-to-llvm='emit-c-wrappers=1' \
+  -reconcile-unrealized-casts | \
+  ${LLVM_MLIR_BINARY_DIR}/mlir-translate --mlir-to-llvmir -o log.ll
